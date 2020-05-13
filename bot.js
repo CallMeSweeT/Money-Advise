176c62c58d1c435f8ce8317c0217c66f0457288d
CallMeSweeT <52930404+CallMeSweeT@users.noreply.github.com>
2020-05-13 10:42:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content === '.discord') {
+    if (message.content === '>>> .discord') {
     	message.reply('https://discord.gg/X6zanqU');
   	}
 });
